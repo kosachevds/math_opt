d716b95c9f86b18aa078b7c69c9ef5b346a168e4
Daniil Kosachev <kos-dan@mail.ru>
2018-10-16 21:27:11 +0300

    Conjugate gradient

--- lab_2/methods.py
+++ lab_2/methods.py
@@ -21,7 +21,7 @@ def steepest_descent(func, gradient_func, x0, eps):
     x_k = x0
     count = 0
     while True:
-        grad_k = gradient_func(x0)
+        grad_k = gradient_func(x_k)
         count += 1
         if la.norm(grad_k) < eps:
             return x_k, count
@@ -29,5 +29,29 @@ def steepest_descent(func, gradient_func, x0, eps):
         def func_alpha(alpha):
             return func([x_k - alpha * grad_k])
 
-        alpha_k = _minimize(func, 0, 1, eps)
+        alpha_k = _minimize(func, eps, 1, eps)
         x_k = x_k - alpha_k * grad_k
+
+
+def conjugate_gradient(func, gradient, x0, eps):
+    x_k = x0
+    grad_k = gradient(x0)
+    p_k = -grad_k
+    count = 1
+    k = 0
+    while True:
+        if la.norm(grad_k) < eps:
+            return x_k, count
+
+        def func_alpha(alpha):
+            return func([x_k + alpha * p_k])
+
+        alpha_k = _minimize(func, eps, 1, eps)
+        x_k = x_k + alpha_k * p_k
+        grad_next = gradient(x_k)
+        beta = (la.norm(grad_next) ** 2) / (la.norm(grad_k) ** 2)
+        if k > 0 and k % len(x0) == 0:
+            beta = 0
+        k += 1
+        grad_k = grad_next
+        p_k = -grad_k + beta * p_k
