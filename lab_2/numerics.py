4a279fa6068a8311ee96a98774091d7fe004c223
Daniil Kosachev <kos-dan@mail.ru>
2018-10-16 21:58:17 +0300

    conjugate_gradient()

--- lab_2/numerics.py
+++ lab_2/numerics.py
@@ -26,3 +26,28 @@ def steepest_descent(func, x0, eps):
 
         alpha_k = onedim.minimize(func, eps, 1, eps)
         x_k = x_k - alpha_k * grad_k
+
+
+def conjugate_gradient(func, x0, eps):
+    x_k = x0
+    grad_k = _numeric_gradient(func, x_k, eps)
+    p_k = -grad_k
+    count = 2 * len(x_k)
+    k = 0
+    while True:
+        if linalg.norm(grad_k) < eps:
+            return x_k, count
+
+        def func_alpha(alpha):
+            return func([x_k + alpha * p_k])
+
+        alpha_k = onedim.minimize(func, eps, 1, eps)
+        x_k = x_k + alpha_k * p_k
+        grad_next = _numeric_gradient(func, x_k, eps)
+        count += 2 * len(x_k)  # 2 calculation for every dim
+        beta = (linalg.norm(grad_next) ** 2) / (linalg.norm(grad_k) ** 2)
+        if k > 0 and k % len(x0) == 0:
+            beta = 0
+        k += 1
+        grad_k = grad_next
+        p_k = -grad_k + beta * p_k
