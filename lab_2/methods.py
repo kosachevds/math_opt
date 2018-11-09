1b9015e0e203131264d09ce4f789a7536f34c026
Daniil <kos-dan@mail.ru>
2018-11-09 21:25:31 +0300

    regular simplex outline

--- lab_2/methods.py
+++ lab_2/methods.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy import linalg
 import onedim
+from simplex import Simplex
 
 
 def steepest_descent(func, gradient_func, x0, eps):
@@ -15,7 +16,7 @@ def steepest_descent(func, gradient_func, x0, eps):
         def func_alpha(alpha):
             return func([x_k - alpha * grad_k])
 
-        alpha_k = onedim.minimize(func, eps, 1, eps)
+        alpha_k = onedim.minimize(func_alpha, eps, 1, eps)
         x_k = x_k - alpha_k * grad_k
 
 
@@ -32,7 +33,7 @@ def conjugate_gradient(func, gradient, x0, eps):
         def func_alpha(alpha):
             return func([x_k + alpha * p_k])
 
-        alpha_k = onedim.minimize(func, eps, 1, eps)
+        alpha_k = onedim.minimize(func_alpha, eps, 1, eps)
         x_k = x_k + alpha_k * p_k
         grad_next = gradient(x_k)
         beta = (linalg.norm(grad_next) ** 2) / (linalg.norm(grad_k) ** 2)
@@ -57,4 +58,17 @@ def nuton(gradient, hessian, x0, eps):
 
 
 def regular_simplex(func, x0, eps):
-    pass
+    simplex = Simplex(x0, eps)
+    simplex.apply(func)
+    while True:
+        simplex.sort()
+        index = simplex.get_count() - 1
+        while index >= 0:
+            x_max, f_max = simplex.get_pair(index)
+            new_x = simplex.get_new_x(x_max)
+            new_f = func(new_x)
+            if new_f < f_max:
+                simplex.replace_pair(index, new_x, new_f)
+                break
+            index -= 1
+        return simplex.nodes[0]
