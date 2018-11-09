1b0a850a270a687e5d63506ad1955b1cc5057214
Daniil <kos-dan@mail.ru>
2018-11-09 21:25:15 +0300

    Some new methods

--- lab_2/simplex.py
+++ lab_2/simplex.py
@@ -34,3 +34,10 @@ class Simplex():
         pairs = sorted(zip(self.nodes, self.values), key=(lambda x: x[1]))
         self.nodes = [p[0] for p in pairs]
         self.values = [p[1] for p in pairs]
+
+    def get_new_x(self, x_opposite):
+        return np.sum(self.nodes) * 2 / len(self.nodes) - x_opposite
+
+    def replace_pair(self, index, new_x, new_f):
+        self.nodes[index] = new_x
+        self.values[index] = new_f
