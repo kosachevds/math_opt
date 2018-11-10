eb875c9e48a92cbddc38e496db619e5a48dd8d85
Daniil <kos-dan@mail.ru>
2018-11-10 18:00:43 +0300

    Add count

--- lab_2/onedim.py
+++ lab_2/onedim.py
@@ -2,15 +2,18 @@ import numpy
 
 
 def minimize(func, begin, end, eps):
+    count = 0
     while end - begin > eps:
         step = (end - begin) / 4.0
         previous = func(begin)
+        count += 1
         for x in numpy.linspace(begin + step, end, num=4):
             current = func(x)
+            count += 1
             if current < previous:
                 previous = current
                 continue
             end = x
             begin = x - step
             break
-    return (begin + end) / 2.0
+    return (begin + end) / 2.0, count
