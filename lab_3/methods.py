747c8c78e2a5acf15017cd6b16cb21a8b3c4721a
Daniil <kos-dan@mail.ru>
2018-11-20 19:47:03 +0300

    Enumerative method

--- lab_3/methods.py
+++ lab_3/methods.py
@@ -1,4 +1,9 @@
-import numpy as np
+import numpy as _np
+
+
+def enumerative(x_list, y_list, z_list):
+    index = _np.argmin(z_list)
+    return (x_list[index], y_list[index])
 
 
 def simulated_annealing():
