799b33309681e8634f60d0d15ca6a411f2d8be2c
Daniil <kos-dan@mail.ru>
2018-11-20 20:03:15 +0300

    Empty functions

--- lab_3/methods.py
+++ lab_3/methods.py
@@ -6,5 +6,17 @@ def enumerative(x_list, y_list, z_list):
     return (x_list[index], y_list[index])
 
 
-def simulated_annealing():
+def random_search(x_list, y_list, z_list, x_0, y_0):
+    pass
+
+
+def pattern_search(x_list, y_list, z_list, x_0, y_0):
+    pass
+
+
+def simulated_annealing(x_list, y_list, z_list, x_0, y_0):
+    pass
+
+
+def genetic_search(x_list, y_list, z_list, x_0, y_0):
     pass
