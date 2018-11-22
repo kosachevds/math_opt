2fa86c85f5d6f76d438db3b31c5ab5ee0661a8bb
Daniil Kosachev <kos-dan@mail.ru>
2018-11-22 22:11:36 +0300

    function plot_errors

--- lab_2/task5.py
+++ lab_2/task5.py
@@ -8,8 +8,9 @@ X_MIN = np.array([1, 1])
 
 def main():
     eps_list = np.logspace(-3, -5, 2)
-    eps_1d_list = np.logspace(-4, -8, 3)
+    eps_1d_list = np.logspace(-4, -8, 5)
     # plot_counts(eps_list, eps_1d_list)
+    plot_errors(eps_list, eps_1d_list)
 
     pp.show()
 
@@ -26,8 +27,27 @@ def plot_counts(eps_list, eps_1d_list):
                            "Steepest descent")
         plot_method_counts(methods.conjugate_gradient, True, eps, eps_1d_list,
                            "Conj. gradients")
-        plot_method_counts(methods.alternating_variable, False, eps, eps_1d_list,
-                           "Alter. variables")
+        plot_method_counts(methods.alternating_variable, False, eps,
+                           eps_1d_list, "Alter. variables")
+
+        pp.gca().invert_xaxis()
+        pp.legend()
+
+
+def plot_errors(eps_list, eps_1d_list):
+    for i, eps in enumerate(eps_list):
+        eps_log = np.log10(eps)
+        pp.figure(i - 1)
+        pp.title("eps^(%f)" % eps_log)
+        pp.ylabel("error")
+        pp.xlabel("onedim eps")
+
+        plot_method_error(methods.steepest_descent, True, eps, eps_1d_list,
+                          "Steepest descent")
+        plot_method_error(methods.conjugate_gradient, True, eps, eps_1d_list,
+                          "Conj. gradients")
+        plot_method_error(methods.alternating_variable, False, eps,
+                          eps_1d_list, "Alter. variables")
 
         pp.gca().invert_xaxis()
         pp.legend()
@@ -59,7 +79,7 @@ def plot_method_error(method, with_gradient, eps, eps_1d_list, label=None):
     else:
         x_list = [method(rosenbrock, X_0, eps, eps_1d)[0]
                   for eps_1d in eps_1d_list]
-    errors = [np.linalg.norm(X_MIN - x_min) for x_min in x_list]
+    errors = np.log10([np.linalg.norm(X_MIN - x_min) for x_min in x_list])
     pp.plot(np.log10(eps_1d_list), errors, label=label)
 
 
