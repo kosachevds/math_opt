6cae2604168baa82607890ca58e1dd883c446332
Daniil Kosachev <kos-dan@mail.ru>
2018-11-22 21:25:39 +0300

    Fixes

--- lab_2/onedim.py
+++ lab_2/onedim.py
@@ -1,24 +1,29 @@
-import numpy as _np
-from matplotlib import pyplot as _pp
+import numpy as np
+from matplotlib import pyplot as pp
 
 
 def minimize(func, begin, end, eps):
-    count = 2
-    if func(begin) < func(begin + eps):
-        return begin, count
-    while end - begin > eps:
+    count = 0
+    # count = 2
+    # if func(begin) < func(begin + eps):
+    #     return begin, count
+    while abs(end - begin) > eps:
         step = (end - begin) / 4.0
+        x_prev = begin
         previous = func(begin)
         count += 1
-        for x in _np.linspace(begin + step, end, num=4):
+        for x in np.linspace(begin + step, end, num=4):
             current = func(x)
             count += 1
             if current < previous:
                 previous = current
+                x_prev = x
                 continue
-            end = x
-            begin = x - step
+            begin = x
+            end = x_prev
             break
+        if x == end:
+            return x, count
     return (begin + end) / 2.0, count
 
 
@@ -40,3 +45,10 @@ def numeric_nuton(func, x_0, eps):
         if dx2 == 0:
             return (None, None)
         x = x - dx1 / dx2
+
+
+def _plot_func(func, begin, end):
+    x_list = np.linspace(begin, end, num=100)
+    f_list = [func(x) for x in x_list]
+    pp.plot(x_list, f_list)
+    pp.show()
